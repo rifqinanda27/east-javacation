9c4afd4d3fec734b43601b138c3ff8fca0aa9999
Wrseno <warsenosetyono@gmail.com>
2024-02-26 21:36:38 +0700

    feat:add city pages

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,101 +1,23 @@
 async function apiWeather() {
-    const res = await fetch(
-        "https://cuaca-gempa-rest-api.vercel.app/weather/jawa-timur/surabaya"
-    );
-    const data = await res.json();
-    const params = data.data.params;
+  let url;
 
-    const currentTimeFormatted = getCurrentTimeFormatted();
-    const currentHour = getCurrentHour();
-    const matchingWeather = [];
+  const pathName = window.location.pathname;
+  if (pathName === "/city/surabaya/" || pathName === "/") {
+    url = "https://cuaca-gempa-rest-api.vercel.app/weather/jawa-timur/surabaya";
+  } else if (pathName === "/city/malang/") {
+    url =
+      "https://cuaca-gempa-rest-api.vercel.app/weather/jawa-timur/kabupaten-malang";
+  } else if (pathName === "/city/pacitan/") {
+    url = "https://cuaca-gempa-rest-api.vercel.app/weather/jawa-timur/pacitan";
+  } else if (pathName === "/city/probolinggo/") {
+    url =
+      "https://cuaca-gempa-rest-api.vercel.app/weather/jawa-timur/kabupaten-probolinggo";
+  } else if (pathName === "/city/kediri/") {
+    url =
+      "https://cuaca-gempa-rest-api.vercel.app/weather/jawa-timur/kabupaten-kediri";
+  }
 
-    params.forEach((param) => {
-        param.times.forEach((item) => {
-            if (
-                parseInt(item.datetime) === parseInt(currentTimeFormatted) &&
-                parseInt(item.h) === parseInt(currentHour)
-            ) {
-                matchingWeather.push(item);
-            }
-        });
-    });
-
-    const weather = document.getElementById("weather");
-
-    weather.innerHTML = " ";
-
-    if (matchingWeather.length > 0) {
-        weather.innerHTML = `
-        <h2 class="text-xl font-bold">${data.data.description}</h2>
-        <div class="flex gap-4">
-            ${matchingWeather
-                .map(
-                    (item) => `
-                ${item.celcius ? `<p class="text-lg">${item.celcius}</p>` : ""}
-                ${item.name
-                            ? `<p class="text-lg">${translateWeather(item.name)}</p>`
-                            : ""
-                        }
-                `
-                )
-                .join("")}
-          </div>
-          <p>The weather is updated every 6 hours.</p>
-    `;
-    } else {
-        weather.innerHTML =
-            "<p>Weather information is not available at the moment.</p>";
-    }
-}
-
-function getCurrentTimeFormatted() {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    let month = currentDate.getMonth() + 1;
-    month = month < 10 ? "0" + month : month;
-    let day = currentDate.getDate();
-    day = day < 10 ? "0" + day : day;
-    let hours = currentDate.getHours();
-    hours = Math.floor(hours / 6) * 6;
-    hours = hours < 10 ? "0" + hours : hours;
-    return `${year}${month}${day}${hours}00`;
-}
-
-function getCurrentHour() {
-    const currentDate = new Date();
-    const hours = (Math.floor(currentDate.getHours() / 6) * 6) % 66;
-
-    if (hours >= 24) {
-        hours = (hours + 6) % 66;
-    }
-
-    return hours < 10 ? "0" + hours : hours;
-}
-
-function translateWeather(weatherInIndonesian) {
-    const weatherTranslations = {
-        Cerah: "Clear Skies",
-        "Cerah Berawan": "Partly Cloudy",
-        Berawan: "Mostly Cloudy",
-        "Berawan Tebal": "Overcast",
-        "Udara Kabut": "Haze",
-        Asap: "Smoke",
-        Kabut: "Fog",
-        "Hujan Ringan": "Light Rain",
-        "Hujan Sedang": "Rain",
-        "Hujan Lebat": "Heavy Rain",
-        "Hujan Lokal": "Isolated Shower",
-        "Hujan Petir": "Severe Thunderstorm",
-    };
-
-    return weatherTranslations[weatherInIndonesian] || weatherInIndonesian;
-}
-
-apiWeather();
-=======
-  const res = await fetch(
-    "https://cuaca-gempa-rest-api.vercel.app/weather/jawa-timur/surabaya"
-  );
+  const res = await fetch(url);
   const data = await res.json();
   const params = data.data.params;
 
@@ -105,10 +27,21 @@ apiWeather();
 
   params.forEach((param) => {
     param.times.forEach((item) => {
-      console.log(item.datetime, item.h, currentHour);
-      if (parseInt(item.datetime) === parseInt(currentTimeFormatted)) {
-        if (parseInt(item.h) === parseInt(currentHour)) {
+      if (currentHour == 6) {
+        if (parseInt(item.datetime) === parseInt(currentTimeFormatted)) {
           matchingWeather.push(item);
+        }
+      }
+      if (
+        currentHour == 24 ||
+        currentHour == 00 ||
+        currentHour == 12 ||
+        currentHour == 18
+      ) {
+        if (parseInt(item.datetime) === parseInt(currentTimeFormatted)) {
+          if (parseInt(item.h) === parseInt(currentHour)) {
+            matchingWeather.push(item);
+          }
         }
       }
     });
@@ -160,12 +93,9 @@ function getCurrentHour() {
   const currentDate = new Date();
   let hours = (Math.floor(currentDate.getHours() / 6) * 6) % 66;
 
-  if (hours === 00) {
-    hours = 24;
-  }
-
-  if (hours > 24) {
-    hours = (hours + 6) % 66;
+  // Mengatasi nilai 0 yang harusnya menjadi 66
+  if (hours === 0) {
+    hours = 66;
   }
 
   return hours < 10 ? "0" + hours : hours;
@@ -191,4 +121,3 @@ function translateWeather(weatherInIndonesian) {
 }
 
 apiWeather();
-
